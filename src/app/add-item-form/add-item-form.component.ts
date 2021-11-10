d12c0bf4b28f39b124781993bc12b6ab878eb887
Michał Płużek <michalpluzek@gmail.com>
2021-11-10 10:41:35 +0100

    Add support for adding new budget item card

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
 
@@ -8,13 +8,15 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
   styleUrls: ['./add-item-form.component.scss'],
 })
 export class AddItemFormComponent implements OnInit {
-  @Input() item: BudgetItem = new BudgetItem();
+  @Input() item: BudgetItem = new BudgetItem('', 0);
+  @Output() formSubmit: EventEmitter<BudgetItem> =
+    new EventEmitter<BudgetItem>();
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    this.formSubmit.emit(form.value);
   }
 }
--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-budget-item-list',
   templateUrl: './budget-item-list.component.html',
-  styleUrls: ['./budget-item-list.component.scss']
+  styleUrls: ['./budget-item-list.component.scss'],
 })
 export class BudgetItemListComponent implements OnInit {
+  @Input() budgetItems: BudgetItem[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
